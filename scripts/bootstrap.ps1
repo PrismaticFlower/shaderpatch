ab0ef0362ea54e15c9a61abf9c68ddae48ace9af
SleepKiller <SleepKiller@users.noreply.github.com>
2018-03-14 02:58:27 +1300

    updated directxtex triplet

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -5,7 +5,7 @@ cd submodules/vcpkg
 
 ./bootstrap-vcpkg.bat
 
-./vcpkg install directxtex:x86-windows
+./vcpkg install directxtex:x86-windows-static-md
 ./vcpkg install glm:x86-windows-static-md
 ./vcpkg install ms-gsl:x86-windows-static-md
 ./vcpkg install nlohmann-json:x86-windows-static-md
