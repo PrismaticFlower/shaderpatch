19c86a97c88b0253d9d815d81ced9016b9f24604
SleepKiller <SleepKiller@users.noreply.github.com>
2018-03-14 23:17:05 +1300

    updated preparepackage script

--- scripts/preparepackage.ps1
+++ scripts/preparepackage.ps1
@@ -7,7 +7,9 @@ md .\packaged
 md .\packaged\data\
 md .\packaged\data\shaderpatch\
 md .\packaged\data\shaderpatch\bin\
-md .\packaged\data\shaderpatch\textures
+md .\packaged\data\shaderpatch\textures\
+md .\packaged\data\shaderpatch\shaders\
+md .\packaged\data\_lvl_pc\
 
 copy .\LICENSE ".\packaged\shader patch license.txt"
 copy .\third_party.md ".\packaged\shader patch acknowledgements.txt"
@@ -28,12 +30,15 @@ if ($installationPath -and (test-path "$installationPath\Common7\Tools\vsdevcmd.
 copy "${installationPath}\VC\Redist\MSVC\14.*\VCRedist_x86.exe" ".\packaged\data\shaderpatch\bin\VCRedist_x86.exe"
 
 # Build
-msbuild /t:Build /p:Configuration=Release /m shader_patch.vcxproj
-msbuild /t:Build /p:Configuration=Release /m tools/installer/installer.csproj
+msbuild /t:Build /p:Configuration=Release /p:Platform=x86 /m shader_patch.sln
 
 copy .\bin\Release\dinput8.dll .\packaged\
 copy ".\bin\Release\shader patch installer.exe" ".\packaged\Shader Patch Installer.exe"
 copy ".\bin\Release\Microsoft.Expression.Drawing.dll" .\packaged\
+
+# Copy over end-user tools.
+copy ".\bin\Release\lvl_pack.exe" ".\packaged\data\shaderpatch\bin\lvl_pack.exe"
+copy ".\bin\Release\shader_compiler.exe" ".\packaged\data\shaderpatch\bin\shader_compiler.exe"
 
 # Copy Assets
 copy '.\assets\shader patch.ini' .\packaged\
@@ -42,6 +47,16 @@ copy '.\assets\shader patch user readme.txt' '.\packaged\shader patch readme.txt
 
 Copy-Item -Path .\assets\textures\* -Destination .\packaged\data\shaderpatch\textures -Recurse
 
-md .\packaged\data\_lvl_pc
+# Copy Shaders.
+Copy-Item -Path .\assets\shaders\* -Destination .\packaged\data\shaderpatch\shaders -Recurse
 
-Write-Host "shader patch package prepared, remember to copy core.lvl over from the shader toolkit!"
+if (Test-Path -Path ".\packaged\data\shaderpatch\shaders\build") 
+{ 
+   del ".\packaged\data\shaderpatch\shaders\build" -Recurse
+}
+
+del ".\packaged\data\shaderpatch\shaders\.gitignore"
+del ".\packaged\data\shaderpatch\shaders\munged\.gitignore"
+del ".\packaged\data\shaderpatch\shaders\munged\*.shader"
+
+Move-Item -Path .\packaged\data\shaderpatch\shaders\core.lvl -Destination .\packaged\data\_lvl_pc\core.lvl
