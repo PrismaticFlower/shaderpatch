81ded9d3c325e2f9e889f9b2434fecd1be1ccbaa
SleepKiller <SleepKiller@users.noreply.github.com>
2018-03-30 00:49:24 +1300

    added .req file parser

---
+++ shared_src/parse_req_file.hpp
@@ -0,0 +1,106 @@
+#pragma once
+
+#include "compose_exception.hpp"
+
+#include <fstream>
+#include <iomanip>
+#include <regex>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+
+namespace sp {
+
+inline auto parse_req_file(boost::filesystem::path filepath)
+   -> std::vector<std::pair<std::string, std::vector<std::string>>>
+{
+   namespace fs = boost::filesystem;
+   using namespace std::literals;
+
+   if (!fs::exists(filepath) || !fs::is_regular_file(filepath)) {
+      throw std::invalid_argument{"Attempt to open non-existent .req file "s};
+   }
+
+   std::string buffer;
+
+   fs::load_string_file(filepath, buffer);
+
+   // Filter out comments.
+   buffer = std::regex_replace(buffer, std::regex{R"(//.+)"s}, ""s);
+
+   std::istringstream stream{buffer};
+
+   std::string header;
+
+   stream >> header;
+
+   if (header != "ucft"sv) {
+      throw compose_exception<std::runtime_error>("Expected \"ucft\" but found "sv,
+                                                  std::quoted(header), '.');
+   }
+
+   char brace;
+
+   stream >> brace;
+
+   if (brace != '{') {
+      throw compose_exception<std::runtime_error>("Expected opening '{' but found '"sv,
+                                                  brace, "'."sv);
+   }
+
+   std::vector<std::pair<std::string, std::vector<std::string>>> section_values;
+
+   while (stream && !stream.eof()) {
+      stream >> header;
+
+      if (header == "}"sv) {
+         break;
+      }
+      else if (!stream) {
+         throw std::runtime_error{"Unexpected end of .req file."s};
+      }
+      else if (header != "REQN"sv) {
+         throw compose_exception<std::runtime_error>("Unexpected token "sv,
+                                                     std::quoted(header),
+                                                     ", expected \"REQN\"");
+      }
+
+      stream >> brace;
+
+      if (brace != '{') {
+         throw compose_exception<std::runtime_error>("Unexpected token '"sv,
+                                                     brace, "', expected '{'"sv);
+      }
+
+      if (!std::getline(stream >> std::ws, buffer, '}')) {
+         throw std::runtime_error{"Unexpected end of .req file."s};
+      }
+
+      std::istringstream section{buffer};
+
+      std::string section_type;
+
+      section >> std::quoted(section_type);
+
+      if (section_type.empty()) {
+         throw std::runtime_error{"Unexpected empty REQN section."s};
+      }
+
+      std::vector<std::string> keys;
+      keys.reserve(32);
+
+      while (section && !section.eof()) {
+         keys.emplace_back();
+
+         section >> std::quoted(keys.back()) >> std::ws;
+      }
+
+      section_values.emplace_back(std::move(section_type), std::move(keys));
+   }
+
+   return section_values;
+}
+}
