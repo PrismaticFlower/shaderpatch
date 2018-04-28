e4b818b3f7d30e6ff2605ced887b6a8829f105af
SleepKiller <SleepKiller@users.noreply.github.com>
2018-04-28 21:48:43 +1200

    added Pandemic cfg parser and manipulator

---
+++ shared_src/config_file.hpp
@@ -0,0 +1,551 @@
+#pragma once
+
+#include "compose_exception.hpp"
+#include "exceptions.hpp"
+#include "string_utilities.hpp"
+
+#include <initializer_list>
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+#include <utility>
+#include <variant>
+#include <vector>
+
+namespace sp::cfg {
+
+class Node {
+public:
+   using value_type = std::pair<std::string, Node>;
+   using reference = value_type&;
+   using const_reference = const value_type&;
+   using iterator = std::vector<std::pair<std::string, Node>>::iterator;
+   using const_iterator = std::vector<std::pair<std::string, Node>>::const_iterator;
+   using difference_type = std::vector<std::pair<std::string, Node>>::difference_type;
+   using size_type = std::vector<std::pair<std::string, Node>>::size_type;
+
+   using Node_value = std::variant<std::string, long long, double>;
+
+   Node() = default;
+
+   Node(size_type count, const_reference value)
+   {
+      _children.assign(count, value);
+   }
+
+   Node(const_iterator first, const_iterator last)
+   {
+      _children.assign(first, last);
+   }
+
+   Node(std::initializer_list<value_type> nodes)
+   {
+      _children.assign(std::cbegin(nodes), std::cend(nodes));
+   }
+
+   auto begin() noexcept -> iterator
+   {
+      return _children.begin();
+   }
+
+   auto begin() const noexcept -> const_iterator
+   {
+      return _children.begin();
+   }
+
+   auto end() noexcept -> iterator
+   {
+      return _children.end();
+   }
+
+   auto end() const noexcept -> const_iterator
+   {
+      return _children.end();
+   }
+
+   auto cbegin() const noexcept -> const_iterator
+   {
+      return _children.cbegin();
+   }
+
+   auto cend() const noexcept -> const_iterator
+   {
+      return _children.cend();
+   }
+
+   auto swap(Node& with) noexcept
+   {
+      std::swap(*this, with);
+   }
+
+   auto size() const noexcept -> size_type
+   {
+      return _children.size();
+   }
+
+   auto max_size() const noexcept -> size_type
+   {
+      return _children.max_size();
+   }
+
+   bool empty() const noexcept
+   {
+      return _children.empty();
+   }
+
+   template<typename... Args>
+   auto emplace(const_iterator where, Args... args) -> iterator
+   {
+      return _children.emplace(where, std::forward<Args>(args)...);
+   }
+
+   auto insert(const_iterator where, const_reference value) -> iterator
+   {
+      return _children.insert(where, value);
+   }
+
+   auto insert(const_iterator where, value_type&& value) -> iterator
+   {
+      return _children.insert(where, std::move(value));
+   }
+
+   auto insert(const_iterator where, size_type count, const_reference value) -> iterator
+   {
+      return _children.insert(where, count, value);
+   }
+
+   auto insert(const_iterator where, const_iterator first, const_iterator last) -> iterator
+   {
+      return _children.insert(where, first, last);
+   }
+
+   auto insert(const_iterator where, std::initializer_list<value_type> nodes) -> iterator
+   {
+      return _children.insert(where, std::cbegin(nodes), std::cend(nodes));
+   }
+
+   auto erase(const_iterator where) -> iterator
+   {
+      return _children.erase(where);
+   }
+
+   auto erase(const_iterator first, const_iterator last) -> iterator
+   {
+      return _children.erase(first, last);
+   }
+
+   void clear()
+   {
+      _children.clear();
+   }
+
+   void assign(const_iterator first, const_iterator last)
+   {
+      return _children.assign(first, last);
+   }
+
+   void assign(std::initializer_list<value_type> nodes)
+   {
+      return _children.assign(std::cbegin(nodes), std::cend(nodes));
+   }
+
+   void assign(size_type count, const_reference value)
+   {
+      return _children.assign(count, value);
+   }
+
+   auto front() noexcept -> reference
+   {
+      return _children.front();
+   }
+
+   auto front() const noexcept -> const_reference
+   {
+      return _children.front();
+   }
+
+   auto back() noexcept -> reference
+   {
+      return _children.back();
+   }
+
+   auto back() const noexcept -> const_reference
+   {
+      return _children.back();
+   }
+
+   template<typename... Args>
+   void emplace_back(Args... args)
+   {
+      _children.emplace_back(std::forward<Args>(args)...);
+   }
+
+   void push_back(const_reference value)
+   {
+      _children.push_back(value);
+   }
+
+   void push_back(value_type&& value)
+   {
+      _children.push_back(std::move(value));
+   }
+
+   void pop_back()
+   {
+      _children.pop_back();
+   }
+
+   template<typename Value_type, typename... Args>
+   void emplace_value(Args... args)
+   {
+      _values.emplace_back(Value_type{std::forward<Args>(args)...});
+   }
+
+   auto values() const noexcept -> const std::vector<Node_value>&
+   {
+      return _values;
+   }
+
+   auto values_count() const noexcept -> size_type
+   {
+      return _values.size();
+   }
+
+   template<typename Value_type>
+   auto get_value(int index = 0) const noexcept -> Value_type
+   {
+      auto val = _values.at(index);
+
+      if constexpr (std::is_arithmetic_v<Value_type>) {
+         if (std::holds_alternative<long long>(val)) {
+            return static_cast<Value_type>(std::get<long long>(val));
+         }
+         else {
+            return static_cast<Value_type>(std::get<double>(val));
+         }
+      }
+      else if (std::is_same_v<Value_type, std::string> ||
+               std::is_same_v<Value_type, std::string_view>) {
+         return static_cast<Value_type>(std::get<std::string>(val));
+      }
+      else {
+         static_assert(false, "Unknown Value_type.");
+      }
+   }
+
+   template<typename Value_type>
+   void set_value(const Value_type& value, int index = 0) noexcept
+   {
+      if constexpr (std::is_floating_point_v<Value_type>) {
+         _values.at(index) = static_cast<double>(value);
+      }
+      else if (std::is_integral_v<Value_type>) {
+         _values.at(index) = static_cast<long long>(value);
+      }
+      else if (std::is_same_v<Value_type, std::string> ||
+               std::is_same_v<Value_type, std::string_view>) {
+         _values.at(index) = static_cast<std::string>(value);
+      }
+      else {
+         static_assert(false, "Unknown Value_type.");
+      }
+   }
+
+   template<typename... Args>
+   void emplace_comment(Args... args) noexcept
+   {
+      _comments.emplace_back(std::forward<Args>(args)...);
+   }
+
+   auto comments() const noexcept -> const std::vector<std::string>&
+   {
+      return _comments;
+   }
+
+   void trailing_comment(std::string_view comment)
+   {
+      _trailing_comment = std::string{comment};
+   }
+
+   auto trailing_comment() const noexcept -> std::string_view
+   {
+      return _trailing_comment;
+   }
+
+private:
+   std::vector<Node_value> _values;
+   std::vector<std::pair<std::string, Node>> _children;
+
+   std::vector<std::string> _comments;
+   std::string _trailing_comment;
+};
+
+void swap(Node& l, Node r) noexcept
+{
+   std::swap(l, r);
+}
+
+namespace detail {
+
+auto parse_key_entry(std::string_view key_entry_comment)
+   -> std::pair<std::string, Node>;
+
+void parse_value(std::string_view value, Node& node);
+
+void parse_scope(std::istream& stream, Node& parent);
+
+struct Indent {
+   Indent(int level) : count{level * width} {}
+
+   constexpr static char ws = ' ';
+   constexpr static int width = 3;
+   int count = 0;
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const Indent& indent);
+
+template<typename Values_range>
+void print_values(std::ostream& stream, const Values_range& values);
+
+template<typename Comments_range>
+void print_comments(std::ostream& stream, int level, const Comments_range& comments);
+
+}
+
+inline Node from_istream(std::istream& stream)
+{
+   using namespace std::literals;
+
+   Node root;
+
+   std::vector<std::string> comments;
+
+   stream >> std::ws;
+
+   for (std::string line; stream.good(); stream >> std::ws) {
+      std::getline(stream, line);
+
+      if (line.empty()) continue;
+
+      if (begins_with(line, "//"sv)) {
+         comments.emplace_back(std::cbegin(line) + 2, std::cend(line));
+      }
+      else if (begins_with(line, "{"sv)) {
+         if (root.empty()) {
+            throw Parse_error{"Unexpected scope start!"s};
+         }
+
+         detail::parse_scope(stream, root.back().second);
+      }
+      else {
+         root.emplace_back(detail::parse_key_entry(line));
+
+         for (auto comment : comments) {
+            root.back().second.emplace_comment(std::move(comment));
+         }
+
+         comments.clear();
+      }
+   }
+
+   return root;
+}
+
+inline std::ostream& to_ostream(std::ostream& stream, const Node& node, int level = 0)
+{
+   using detail::Indent;
+   using namespace std::literals;
+
+   for (auto& child : node) {
+      detail::print_comments(stream, level, child.second.comments());
+
+      stream << Indent{level} << child.first << '(';
+
+      detail::print_values(stream, child.second.values());
+
+      stream << ')';
+
+      if (!child.second.trailing_comment().empty()) {
+         stream << " // "sv << child.second.trailing_comment();
+      }
+
+      stream << '\n';
+
+      if (child.second.size() == 0) continue;
+
+      stream << Indent{level} << "{\n"sv;
+
+      to_ostream(stream, child.second, level + 1);
+
+      stream << Indent{level} << "}\n\n"sv;
+   }
+
+   return stream;
+}
+
+inline std::istream& operator>>(std::istream& stream, Node& node)
+{
+   node = from_istream(stream);
+}
+
+inline std::ostream& operator<<(std::ostream& stream, const Node& node)
+{
+   return to_ostream(stream, node, 0);
+}
+
+namespace detail {
+
+inline auto parse_key_entry(std::string_view key_entry_comment)
+   -> std::pair<std::string, Node>
+{
+   using namespace std::literals;
+
+   Node node;
+
+   auto [key_entry, comment] = split_string_on(key_entry_comment, "//"sv);
+
+   node.trailing_comment(comment);
+
+   auto [key, trail] = seperate_string_at(key_entry, "("sv);
+   key = trim_whitespace(key);
+
+   auto values_trail = sectioned_split_split(trail, "("sv, ")"sv);
+
+   if (!values_trail) {
+      throw compose_exception<Parse_error>("Error parsing key "sv,
+                                           std::quoted(key), '.');
+   }
+
+   auto values = tokenize_string_on(values_trail.value().at(0), ","sv);
+
+   for (auto value : values) {
+      try {
+         parse_value(value, node);
+      }
+      catch (std::exception& e) {
+         throw compose_exception<Parse_error>("Error parsing key "sv,
+                                              std::quoted(key), ' ', e.what());
+      }
+   }
+
+   return {std::string{key}, node};
+}
+
+inline void parse_value(std::string_view value, Node& node)
+{
+   using namespace std::literals;
+
+   value = trim_whitespace(value);
+
+   if (begins_with(value, "\""sv)) {
+      if (!ends_with(value, "\""sv)) {
+         throw compose_exception<Parse_error>("String missing closing quote."sv);
+      }
+
+      value.remove_prefix(1);
+      value.remove_suffix(1);
+
+      node.emplace_value<std::string>(value);
+   }
+   else if (contains(value, "."sv)) {
+      node.emplace_value<double>(std::stod(std::string{value}));
+   }
+   else {
+      node.emplace_value<long long>(std::stoll(std::string{value}));
+   }
+}
+
+inline void parse_scope(std::istream& stream, Node& parent)
+{
+   using namespace std::literals;
+
+   std::vector<std::string> comments;
+
+   stream >> std::ws;
+
+   for (std::string line; stream.good(); stream >> std::ws) {
+      std::getline(stream, line);
+
+      if (line.empty()) continue;
+
+      if (begins_with(line, "//"sv)) {
+         comments.emplace_back(std::cbegin(line) + 2, std::cend(line));
+      }
+      else if (begins_with(line, "{"sv)) {
+         if (parent.empty()) {
+            throw Parse_error{"Unexpected scope start!"s};
+         }
+
+         parse_scope(stream, parent.back().second);
+      }
+      else if (begins_with(line, "}"sv)) {
+         return;
+      }
+      else {
+         parent.emplace_back(detail::parse_key_entry(line));
+
+         for (auto comment : comments) {
+            parent.back().second.emplace_comment(std::move(comment));
+         }
+
+         comments.clear();
+      }
+   }
+
+   throw Parse_error{"Reached end of stream inside scope without finding closing bracket!"s};
+}
+
+inline std::ostream& operator<<(std::ostream& stream, const Indent& indent)
+{
+   for (int i = 0; i < indent.count; ++i) {
+      stream.put(indent.ws);
+   }
+
+   return stream;
+}
+
+template<typename Values_range>
+void print_values(std::ostream& stream, const Values_range& values)
+{
+   bool first = true;
+
+   for (const auto& v : values) {
+      if (!std::exchange(first, false)) {
+         stream << ", "sv;
+      }
+
+      std::visit(
+         [&](auto val) {
+            using Type = std::decay_t<decltype(val)>;
+
+            if constexpr (std::is_same_v<Type, long long>) {
+               stream << val;
+            }
+            else if constexpr (std::is_same_v<Type, double>) {
+               stream << std::setprecision(5) << val;
+            }
+            else if constexpr (std::is_same_v<Type, std::string>) {
+               stream << std::quoted(val);
+            }
+            else
+
+            {
+               static_assert(false, "Unexpected Node_value type!");
+            }
+         },
+         v);
+   }
+}
+
+template<typename Comments_range>
+void print_comments(std::ostream& stream, int level, const Comments_range& comments)
+{
+   for (const auto& comment : comments) {
+      stream << Indent{level} << "// "sv << comment << '\n';
+   }
+}
+}
+
+}
--- shared_src/string_utilities.hpp
+++ shared_src/string_utilities.hpp
@@ -4,16 +4,25 @@
 #include <cctype>
 #include <functional>
 #include <iosfwd>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <type_traits>
+#include <vector>
 
 #include <gsl/gsl>
 
 namespace sp {
 
+//! \brief Splits a string along a delimiter, exclusively.
+//!
+//! \param string The string view to split.
+//! \param delimiter The delimiter to split the string on.
+//!
+//! \return Two string views of either side of the delimiter. If the delimiter
+//! was not found, the second view is empty.
 template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
-constexpr auto split_string(
+constexpr auto split_string_on(
    std::basic_string_view<Char_t, Char_triats> string,
    typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type delimiter) noexcept
    -> std::array<std::basic_string_view<Char_t, Char_triats>, 2>
@@ -31,22 +40,271 @@ constexpr auto split_string(
    return split_strings;
 }
 
+//! \brief Splits a string along a delimiter, exclusively.
+//!
+//! \param string The string to split.
+//! \param delimiter The delimiter to split the string on.
+//!
+//! \return Two string views of either side of the delimiter. If the delimiter
+//! was not found, the second view is empty.
 template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
-constexpr auto split_string(
-   const Char_t* string,
-   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type delimiter) noexcept
-   -> std::array<std::basic_string_view<Char_t, Char_triats>, 2>
-{
-   return split_string(std::basic_string_view<Char_t, Char_triats>{string}, delimiter);
-}
-
-template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
-inline auto split_string(
+constexpr auto split_string_on(
    const std::basic_string<Char_t, Char_triats>& string,
    typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type delimiter) noexcept
    -> std::array<std::basic_string_view<Char_t, Char_triats>, 2>
 {
-   return split_string(std::basic_string_view<Char_t, Char_triats>{string}, delimiter);
+   return split_string_on(std::basic_string_view<Char_t, Char_triats>{string}, delimiter);
+}
+
+//! \brief Splits a string along a delimiter, inclusively. The delimiter
+//! will be included in the second returned string.
+//!
+//! \param string The string view to split.
+//! \param delimiter The delimiter to split the string on.
+//!
+//! \return Two string views of either side of the delimiter. If the delimiter
+//! was not found, the second view is empty.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr auto seperate_string_at(
+   std::basic_string_view<Char_t, Char_triats> string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type delimiter) noexcept
+   -> std::array<std::basic_string_view<Char_t, Char_triats>, 2>
+{
+   const auto offset = string.find_first_of(delimiter);
+
+   if (offset == string.npos) return {string, std::string_view{""}};
+
+   std::array<std::basic_string_view<Char_t, Char_triats>, 2> split_strings;
+
+   split_strings[0] = string.substr(0, offset);
+   split_strings[1] = string.substr(offset, string.length() - offset);
+
+   return split_strings;
+}
+
+//! \brief Splits a string along a delimiter, inclusively. The delimiter
+//! will be included in the second returned string.
+//!
+//! \param string The string to split.
+//! \param delimiter The delimiter to split the string on.
+//!
+//! \return Two string views of either side of the delimiter. If the delimiter
+//! was not found, the second view is empty.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr auto seperate_string_at(
+   const std::basic_string<Char_t, Char_triats>& string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type delimiter) noexcept
+   -> std::array<std::basic_string_view<Char_t, Char_triats>, 2>
+{
+   return seperate_string_at(std::basic_string_view<Char_t, Char_triats>{string},
+                             delimiter);
+}
+
+//! \brief Splits a string along a delimiter, repeatadly and exclusively.
+//!
+//! \param string The string view to split.
+//! \param delimiter The delimiter to split the string on.
+//!
+//! \return A vector string views split along the delimiter.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+inline auto tokenize_string_on(
+   std::basic_string_view<Char_t, Char_triats> string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type delimiter)
+   -> std::vector<std::string_view>
+{
+   std::vector<std::string_view> parts;
+
+   for (auto offset = string.find_first_of(delimiter); offset != string.npos;
+        offset = string.find_first_of(delimiter)) {
+
+      parts.emplace_back(string.substr(0, offset));
+      string = string.substr(offset + delimiter.length(),
+                             string.length() - offset - delimiter.length());
+   }
+
+   if (!string.empty()) parts.emplace_back(string);
+
+   return parts;
+}
+
+//! \brief Splits a string along a delimiter, repeatadly and exclusively.
+//!
+//! \param string The string view to split.
+//! \param delimiter The delimiter to split the string on.
+//!
+//! \return A vector string views split along the delimiter.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+inline auto tokenize_string_on(
+   const std::basic_string<Char_t, Char_triats>& string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type delimiter) noexcept
+   -> std::vector<std::string_view>
+{
+   return tokenize_string_on(std::basic_string_view<Char_t, Char_triats>{string},
+                             delimiter);
+}
+
+//! \brief Checks if a string begins with another string.
+//!
+//! \param string The string view to split.
+//! \param what The string to search check for.
+//!
+//! \return True if the string begeins with what, false if it doesn't.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr bool begins_with(
+   std::basic_string_view<Char_t, Char_triats> string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type what) noexcept
+{
+   if (what.size() > string.size()) return false;
+
+   return (string.substr(0, what.size()) == what);
+}
+
+//! \brief Checks if a string begins with another string.
+//!
+//! \param string The string to split.
+//! \param what The string to search check for.
+//!
+//! \return True if the string ends with what, false if it doesn't.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr bool begins_with(
+   const std::basic_string<Char_t, Char_triats>& string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type what) noexcept
+{
+   return begins_with(std::basic_string_view<Char_t, Char_triats>{string}, what);
+}
+
+//! \brief Checks if a string ends with another string.
+//!
+//! \param string The string view to split.
+//! \param what The string to search check for.
+//!
+//! \return True if the string begeins with what, false if it doesn't.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr bool ends_with(
+   std::basic_string_view<Char_t, Char_triats> string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type what) noexcept
+{
+   if (what.size() > string.size()) return false;
+
+   return (string.substr(string.size() - what.size(), what.size()) == what);
+}
+
+//! \brief Checks if a string ends with another string.
+//!
+//! \param string The string to split.
+//! \param what The string to search check for.
+//!
+//! \return True if the string ends with what, false if it doesn't.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr bool ends_with(
+   const std::basic_string<Char_t, Char_triats>& string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type what) noexcept
+{
+   return begins_with(std::basic_string_view<Char_t, Char_triats>{string}, what);
+}
+
+//! \brief Checks if a string contains another string.
+//!
+//! \param string The string view to split.
+//! \param what The string to search check for.
+//!
+//! \return True if the string contains what, false if it doesn't.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr bool contains(
+   std::basic_string_view<Char_t, Char_triats> string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type what) noexcept
+{
+   return (string.find(what) != string.npos);
+}
+
+//! \brief Checks if a string contains another string.
+//!
+//! \param string The string to split.
+//! \param what The string to search check for.
+//!
+//! \return True if the string contains what, false if it doesn't.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr bool contains(
+   const std::basic_string<Char_t, Char_triats>& string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type what) noexcept
+{
+   return contains(std::basic_string_view<Char_t, Char_triats>{string}, what);
+}
+
+//! \brief Splits a string into two parts using section delimiters.
+//!
+//! \param string The string view to split.
+//! \param open The opening delimiter for the section. The string must start
+//! with this.
+//! \param close The closing delimiter for the section.
+//!
+//! \return An optional value containing the resulting strings if the split
+//! was successful.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr auto sectioned_split_split(
+   std::basic_string_view<Char_t, Char_triats> string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type open,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type close = open) noexcept
+   -> std::optional<std::array<std::basic_string_view<Char_t, Char_triats>, 2>>
+{
+   if (!begins_with(string, open)) return std::nullopt;
+
+   string = split_string_on(string, open)[1];
+
+   auto [section, remainder] = seperate_string_at(string, close);
+
+   if (!begins_with(remainder, close)) return std::nullopt;
+
+   remainder.remove_prefix(close.size());
+
+   return std::array<std::basic_string_view<Char_t, Char_triats>, 2>{section, remainder};
+}
+
+//! \brief Splits a string into two parts using section delimiters.
+//!
+//! \param string The string view to split.
+//! \param open The opening delimiter for the section. The string must start
+//! with this.
+//! \param close The closing delimiter for the section.
+//!
+//! \return An optional value containing the resulting strings if the split
+//! was successful.
+template<typename Char_t, typename Char_triats = std::char_traits<Char_t>>
+constexpr auto sectioned_split_split(
+   std::basic_string<Char_t, Char_triats> string,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type open,
+   typename std::common_type<std::basic_string_view<Char_t, Char_triats>>::type close) noexcept
+   -> std::optional<std::array<std::basic_string_view<Char_t, Char_triats>, 2>>
+{
+   return sectioned_split_split(std::basic_string_view<Char_t, Char_triats>{string},
+                                open, close);
+}
+
+//! \brief Trim leading and trailing whitespace from a string view.
+//!
+//! \param string The string view to trim whitespace from.
+//!
+//! \return The resulting string view.
+template<typename Char_triats = std::char_traits<char>>
+constexpr auto trim_whitespace(std::basic_string_view<char, Char_triats> string) noexcept
+   -> std::basic_string_view<char, Char_triats>
+{
+   const auto not_ws = [](auto c) { return std::isspace(c) == 0; };
+
+   {
+      auto result = std::find_if(std::begin(string), std::end(string), not_ws);
+
+      string.remove_prefix(
+         gsl::narrow_cast<std::size_t>(std::distance(std::begin(string), result)));
+   }
+   {
+      auto result = std::find_if(std::rbegin(string), std::rend(string), not_ws);
+
+      string.remove_suffix(
+         gsl::narrow_cast<std::size_t>(std::distance(std::rbegin(string), result)));
+   }
+
+   return string;
 }
 
 struct Ci_char_traits : public std::char_traits<char> {
