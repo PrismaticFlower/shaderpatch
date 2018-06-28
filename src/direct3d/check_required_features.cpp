6318bbaf634d81896e743aed57b6dfbfccebd49c
SleepKiller <SleepKiller@users.noreply.github.com>
2018-06-28 23:47:45 +1200

    cleaned up check_required_features

--- src/direct3d/check_required_features.cpp
+++ src/direct3d/check_required_features.cpp
@@ -17,35 +17,52 @@ constexpr auto failed_message =
    "\nPress Try Again to break into a debugger."
    "\nPress Continue to ignore the missing features and attempt to continue.";
 
-bool feature_test(const bool result, std::string_view error_msg)
-{
-   if (!result) log(Log_level::error, error_msg);
-
-   return result;
-}
+constexpr auto ati1_format = static_cast<D3DFORMAT>(MAKEFOURCC('A', 'T', 'I', '1'));
+constexpr auto ati2_format = static_cast<D3DFORMAT>(MAKEFOURCC('A', 'T', 'I', '2'));
 
 bool supports_texture_format(IDirect3D9& d3d, D3DFORMAT format)
 {
    return (SUCCEEDED(d3d.CheckDeviceFormat(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, D3DFMT_X8R8G8B8,
                                            0, D3DRTYPE_TEXTURE, format)) == 1);
 }
+
+bool supports_render_texture_format(IDirect3D9& d3d, D3DFORMAT format)
+{
+   return (SUCCEEDED(d3d.CheckDeviceFormat(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL,
+                                           D3DFMT_X8R8G8B8, D3DUSAGE_RENDERTARGET,
+                                           D3DRTYPE_TEXTURE, format)) == 1);
+}
+
 }
 
 void check_required_features(IDirect3D9& d3d) noexcept
 {
    bool supported = true;
 
+   const auto feature_test = [&supported](bool test_result,
+                                          std::string_view error_message) {
+      if (!test_result) log(Log_level::error, error_message);
+
+      supported &= test_result;
+   };
+
    D3DCAPS9 caps;
 
    d3d.GetDeviceCaps(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, &caps);
 
-   supported &= feature_test(caps.VertexShaderVersion >= 0xfffe0300u,
-                             "Device does not support Shader Model 3.0."sv);
-   supported &= feature_test(caps.PixelShaderVersion >= 0xfffe0300u,
-                             "Device does not support Shader Model 3.0."sv);
+   feature_test(caps.VertexShaderVersion >= 0xfffe0300u,
+                "Device does not support Shader Model 3.0."sv);
+   feature_test(caps.PixelShaderVersion >= 0xfffe0300u,
+                "Device does not support Shader Model 3.0."sv);
 
-   supported &= feature_test(supports_texture_format(d3d, D3DFMT_DXT5),
-                             "Device does not support BC3 (DXT(4|5) textures."sv);
+   feature_test(supports_texture_format(d3d, D3DFMT_DXT1),
+                "Device does not support BC1 (DXT1) textures."sv);
+   feature_test(supports_texture_format(d3d, D3DFMT_DXT5),
+                "Device does not support BC3 (DXT(4|5) textures."sv);
+   feature_test(supports_texture_format(d3d, ati1_format),
+                "Device does not support ATI1 (BC4) textures."sv);
+   feature_test(supports_texture_format(d3d, ati2_format),
+                "Device does not support ATI2 textures."sv);
 
    if (!supported) {
       const auto action = MessageBoxA(nullptr, failed_message, "Device Unsupported",
