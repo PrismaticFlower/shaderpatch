204cb2540b7de2230129c57ff31c099621a2dfc1
SleepKiller <SleepKiller@users.noreply.github.com>
2020-01-30 20:47:29 +1300

    adjust default SSAO params

--- src/effects/postprocess_params.hpp
+++ src/effects/postprocess_params.hpp
@@ -102,9 +102,9 @@ struct Film_grain_params {
 struct SSAO_params {
    bool enabled = true;
 
-   float radius = 1.2f;
-   float shadow_multiplier = 1.0f;
-   float shadow_power = 1.2f;
+   float radius = 1.5f;
+   float shadow_multiplier = 0.75f;
+   float shadow_power = 0.75f;
    float detail_shadow_strength = 0.5f;
    int blur_pass_count = 2;
    float sharpness = 0.98f;
