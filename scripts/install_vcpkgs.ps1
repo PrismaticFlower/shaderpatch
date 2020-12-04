687983948e548a76bf5cbce0dfd3795771c2410a
SleepKiller <SleepKiller@users.noreply.github.com>
2020-12-05 03:31:46 +1300

    statically link libraries for x86

--- scripts/install_vcpkgs.ps1
+++ scripts/install_vcpkgs.ps1
@@ -1,5 +1,5 @@
 $Packages = "directxtex", "directxmesh", "openexr", "zlib", "stb", "glm", "ms-gsl", "nlohmann-json", "clara", "yaml-cpp", "freetype"
-$x86Packages = $Packages + ("detours", "abseil", "fmt")
+$x86Packages = $Packages + ("detours", "abseil[cxx17]", "fmt")
 
 ./vcpkg install --triplet x64-windows $Packages
-./vcpkg install --triplet x86-windows $x86Packages
+./vcpkg install --triplet x86-windows-static-md $x86Packages
