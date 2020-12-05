071eace0bed3388ff2681e451f15af1c38c2b9ce
SleepKiller <SleepKiller@users.noreply.github.com>
2020-12-05 15:12:39 +1300

    rescale cas input params

--- src/effects/control.cpp
+++ src/effects/control.cpp
@@ -599,11 +599,15 @@ FFX_cas_params show_ffx_cas_imgui(FFX_cas_params params) noexcept
    // The hope is this has the effect of making people observe and consider the trade off of increased
    // sharpness vs ringing when configuring CAS. Or maybe it won't and this is a waste of time.
 
-   float fidelity = 1.0f - std::clamp(params.sharpness, 0.0f, 1.0f);
+   constexpr float max_sharpness = 0.4f;
 
-   ImGui::DragFloat("Fidelity", &fidelity, 0.025f, 0.0f, 1.0f);
+   float fidelity =
+      1.0f - (std::clamp(params.sharpness, 0.0f, max_sharpness) / max_sharpness);
 
-   params.sharpness = 1.0f - fidelity;
+   ImGui::DragFloat("Fidelity", &fidelity, 0.01f, 0.0f, 1.0f);
+
+   params.sharpness =
+      std::lerp(0.0f, max_sharpness, std::clamp(1.0f - fidelity, 0.0f, 1.0f));
 
    return params;
 }
