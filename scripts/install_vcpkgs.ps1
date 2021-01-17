31b0875206a415b4e0952f989fe6c6d41cff69d8
SleepKiller <SleepKiller@users.noreply.github.com>
2021-01-17 17:45:19 +1300

    static link all the things

--- scripts/install_vcpkgs.ps1
+++ scripts/install_vcpkgs.ps1
@@ -1,7 +1,5 @@
-$Packages = "directxtex", "directxmesh", "openexr", "zlib", "stb", "glm", "ms-gsl", "nlohmann-json", "clara", "yaml-cpp", "abseil[cxx17]"
-$x86Packages = $Packages + ("detours", "freetype", "fmt", "sol2", "lua[cpp]")
-$x86ManagerPackages = "yaml-cpp"
+$Packages = "directxtex", "directxmesh", "openexr", "zlib", "stb", "glm", "ms-gsl", "nlohmann-json", "clara", "yaml-cpp", "freetype", "abseil[cxx17]", "fmt"
+$x86Packages = $Packages + ("detours", "lua[cpp]", "sol2")
 
-./vcpkg install --triplet x64-windows $Packages
-./vcpkg install --triplet x86-windows-static-md $x86Packages
-./vcpkg install --triplet x86-windows-static $x86ManagerPackages
+./vcpkg install --triplet x64-windows-static $Packages
+./vcpkg install --triplet x86-windows-static $x86Packages
