7c3dc8b49ae939c92bd02dbb4393d9d79a76a44d
SleepKiller <SleepKiller@users.noreply.github.com>
2022-05-14 18:49:04 +1200

    use vcpkg manifest

--- scripts/install_vcpkgs.ps1
+++
@@ -1,5 +0,0 @@
-$Packages = "directxtex", "directxmesh", "openexr", "zlib", "stb", "glm", "ms-gsl", "nlohmann-json", "clara", "yaml-cpp", "freetype", "abseil[cxx17]", "fmt"
-$x86Packages = $Packages + ("detours", "lua[cpp]", "sol2")
-
-./vcpkg install --triplet x64-windows-static $Packages
-./vcpkg install --triplet x86-windows-static $x86Packages
